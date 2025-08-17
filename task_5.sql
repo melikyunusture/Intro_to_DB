a6e856265aeab09ceef9ad90a48f5de5a2168ec2
Melik Yunus <melikact34@gmail.com>
2025-08-17 13:17:28 +0300

    populating table

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,3 @@
 INSERT INTO customers (customer_id, customer_name, email, address)
-VALUES (1, 'Melik Yunus', 'melikact34@gmail.com', 'adama-ethiopia');
+VALUES (1, "Melik Yunus", "melikact34@gmail.com", "adama-ethiopia");
+
